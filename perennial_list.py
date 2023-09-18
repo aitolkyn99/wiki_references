be0804fe76cf4e21c58ecee3b51f16644faac9e2
aitolkyn <aitolkyn.b@kaist.ac.kr>
2023-09-18 16:53:54 +0900

    add script to parse perennial sources table

---
+++ perennial_list.py
@@ -0,0 +1,90 @@
+from importlib.machinery import SOURCE_SUFFIXES
+from unittest.case import DIFF_OMITTED
+from winreg import DisableReflectionKey
+import pandas as pd 
+import requests 
+from bs4 import BeautifulSoup
+import pickle
+
+# Function to parse perennial source table to a pandas dataframe 
+def get_source_table(table):
+    records = []
+    columns = ['name','status','List','Last','Summary','source']
+    for tr in table.findAll("tr"):
+        ths = tr.findAll("th")
+        if ths != []:
+            continue
+        else:
+            trs = tr.findAll("td")
+            record = []
+            urls = []
+            for i, each in enumerate(trs):
+                try:
+                    if 'Special:Search/insource' in each.a['title']:
+                        for a in each.findAll('a'):
+                            if 'Special:Search/insource' in a['title']:
+                                link = a['href']
+                                link = link.replace('/wiki/Special:Search/insource:%22','').replace('%22','')
+                                urls.append(link)
+                    elif i == 1:
+                        text = each.a['title']
+                        record.append(text)
+                    else:
+                        text = each.text.replace('\n','')
+                        record.append(text)
+                except:
+                    if len(record)==5:
+                        continue
+                    else:
+                        text = each.text
+                        record.append(text)
+            for url in urls:
+                tmp = record.copy()
+                tmp.append(url)
+                records.append(tmp)
+
+            # Three special cases loaded previously
+            if len(urls)==0 and record[0]=='Peerage websites (self-published)':
+                for url in peerage:
+                    tmp = record.copy()
+                    tmp.append(url)
+                    records.append(tmp)
+            if record[0] == 'Advameg (City-Data)':
+                for url in advameg:
+                    tmp = record.copy()
+                    tmp.append(url)
+                    records.append(tmp)
+            elif record[0] == 'Natural News (NewsTarget)':
+                for url in newstarget:
+                    tmp = record.copy()
+                    tmp.append(url)
+                    records.append(tmp)
+            
+    df = pd.DataFrame(data = records, columns = columns)
+    df = df[df.source.str.contains('\.')] # this is a simple check for broken urls; May need more rigorous check depending on the list version
+    return df
+
+
+wikiurl="https://en.wikipedia.org/wiki/Wikipedia:Reliable_sources/Perennial_sources" #link to the English Wiki's perennial sources list
+table_class="wikitable sortable perennial-sources jquery-tablesorter"
+response=requests.get(wikiurl)
+# print(response.status_code) # check the status
+soup = BeautifulSoup(response.text, 'html.parser')
+table=soup.find('table',{'class':"wikitable"})
+
+# Currently URLs that belong to "Advameg", "Natural News (NewsTarget)", and "Peerage websites" are listed separately. So, I collect them separately in the 'data' directory
+# Below I load the lists
+
+advameg = []
+newstarget = []
+peerage = [] 
+
+with open('data/advameg.pkl', 'rb') as f:
+    advameg = pickle.load(f)
+with open('data/newstarget.pkl', 'rb') as f:
+    newstarget = pickle.load(f)
+with open('data/peerage.pkl', 'rb') as f:
+    peerage = pickle.load(f)
+
+
+perennials_df = get_source_table(table)
